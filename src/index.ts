fc34dae33a8cafc0e094e39eca3df091cf6830ef
Alvaro-78 <alvaro78an@gmail.com>
2024-01-27 19:39:12 +0100

    Fixed index

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import 'reflect-metadata';
 import fastify, { FastifyInstance } from 'fastify';
 import cors from '@fastify/cors';
+import { AppDataSource } from './db-connection';
 
 const app: FastifyInstance = fastify({ logger: true });
 
-// interface IQuery {
-// 	name: string;
-// }
+async function main() {
+	await AppDataSource.initialize();
+	app.listen({ port: 3001, host: '0.0.0.0' }, (err, address) => {
+		if (err) {
+			app.log.error(err);
+			process;
+		}
+		app.log.info(`server listening on ${address}`);
+	});
+}
 
-// interface IHeaders {
-// 	'x-token': string;
-// }
-
-// interface IReply {
-// 	code: number;
-// 	message: string;
-// 	body: any;
-// }
+main();
 
 app.register(cors, {
 	origin: '*',
@@ -27,10 +27,3 @@ app.get('/', (request, reply) => {
 	reply.send({ hello: 'Álvaro' });
 });
 
-app.listen({ port: 3001, host: '0.0.0.0' }, (err, address) => {
-	if (err) {
-		app.log.error(err);
-		process;
-	}
-	app.log.info(`server listening on ${address}`);
-});
