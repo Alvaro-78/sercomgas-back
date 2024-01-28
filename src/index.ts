c086f76a1bc61cea6d5ac4b0d5141249a04e0bd3
Alvaro-78 <alvaro78an@gmail.com>
2024-01-28 11:03:58 +0100

    Past project to node-express

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,4 @@
-import 'reflect-metadata';
-import fastify, { FastifyInstance } from 'fastify';
-import cors from '@fastify/cors';
-import { AppDataSource } from './db-connection';
+import app from './app';
 
-const app: FastifyInstance = fastify({ logger: true });
-
-async function main() {
-	await AppDataSource.initialize();
-	app.listen({ port: 3001, host: '0.0.0.0' }, (err, address) => {
-		if (err) {
-			app.log.error(err);
-			process;
-		}
-		app.log.info(`server listening on ${address}`);
-	});
-}
-
-main();
-
-app.register(cors, {
-	origin: '*',
-	methods: ['GET', 'POST'],
-});
-
-app.get('/', (request, reply) => {
-	reply.send({ hello: 'Álvaro' });
-});
-
+app.listen(3001);
+console.log('Server is listening on port, 3001');
